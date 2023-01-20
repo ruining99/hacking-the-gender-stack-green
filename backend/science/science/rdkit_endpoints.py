dd82d7aa09ed66a35b3f7f8369fe2cdeec69ea69
Hannah Q. Kim <hk2684@nyu.edu>
2023-01-20 20:07:49 +0000

    added prop function

--- backend/science/science/rdkit_endpoints.py
+++ backend/science/science/rdkit_endpoints.py
@@ -51,3 +51,32 @@ def valid(mol_smi: str):
         return True
     else:
         return False
+
+
+def get_props(str):
+    mol = Chem.MolFromSmiles(str)
+    props = QED.properties(mol)
+    #     props_str = str(props)
+    split_props = props_str.split()
+
+    # Hydrogen bond donors (No more than 5 hydrogen bond donors)
+    HBD0 = split_props[3]
+    HBD1 = HBD0.split("=")
+    HBD = HBD1[1]
+
+    # Hydrogen bond acceptors (No more than 10 hydrogen bond acceptors)
+    HBA0 = split_props[2]
+    HBA1 = HBA0.split("=")
+    HBA = HBA1[1]
+
+    # Molecular mass (A molecular mass less than 500)
+    MW0 = split_props[0]
+    MW1 = MW0.split("=")
+    MW = MW1[1]
+
+    # Octanol-water partition coefficient (does not exceed 5)
+    ALOGP0 = split_props[1]
+    ALOGP1 = ALOGP0.split("=")
+    ALOGP = ALOGP1[1]
+
+    return f"This molecule has {HBD[:-1]} hydrogen bond donors, {HBA[:-1]} hydrogen bond acceptors. The molecular mass is {MW[:-1]} and the ALOGP is {ALOGP[:-1]}"
