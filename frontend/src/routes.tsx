4133cbd6ac6ea8b0807a28031cfc4e76ed3b03b8
jal1077 <95436347+jal1077@users.noreply.github.com>
2023-01-20 20:57:52 +0000

    frontend

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,14 @@
 import { Outlet, Route, createRoutesFromElements } from 'react-router';
 
 import ErrorPage from './error-page/ErrorPage';
+import Grammarly from './grammarly/grammarly.jsx';
 import Home from './home/Home';
 import { withMainLayout } from './layout/main/Main';
+
 
 export default createRoutesFromElements(
   <Route path="/" element={withMainLayout(<Outlet />)} errorElement={withMainLayout(<ErrorPage />)}>
     <Route index element={<Home />} />
+    <Route index path='grammarly' element={<Grammarly />} />
   </Route>
 );
